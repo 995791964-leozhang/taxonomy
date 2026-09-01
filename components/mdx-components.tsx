360ce956998b85b52cea46c23659fdd294c819c4
995791964-leozhang <995791964@qq.com>
2026-09-01 12:18:03 +0800

    Update mdx-components.tsx

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
-import Image from "next/image"
+import NextImage, { ImageProps } from "next/image"
 import { useMDXComponent } from "next-contentlayer/hooks"
 
 import { cn } from "@/lib/utils"
 import { Callout } from "@/components/callout"
 import { MdxCard } from "@/components/mdx-card"
-
+const Image = (props: ImageProps) => {
+  return <NextImage {...props} />
+}
 const components = {
   h1: ({ className, ...props }) => (
     <h1
